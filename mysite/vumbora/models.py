c7e38a472aa40d3cc0c4159559a25b813e37bafe
Matheus da Silva Dantas <20222014040018@ifrn.local>
2023-11-07 17:28:09 -0300

    consertar models

--- mysite/vumbora/models.py
+++ mysite/vumbora/models.py
@@ -1,24 +1,28 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Usuario(models.Model):
-    usuario_nome: models.CharField(max_length=200);
-    usuario_email: models.EmailField(max_length=254);
-    usuario_tipo:models.CharField(max_length=14, choices=[
+    usuario_nome = models.CharField(max_length=200, default='SOME STRING')
+    usuario_email = models.EmailField(max_length=200, default='SOME STRING')
+    usuario_tipo = models.CharField(max_length=14,default = 'F' ,choices=[
         ('F', 'Física'),
         ('J', 'Jurídica')
-    ]);
+    ])
+    
+
+class Local(models.Model):
+    local_estacionamento = models.BooleanField(default =True)
+    local_bairro = models.CharField(max_length=200)
+    local_cep = models.CharField(max_length=8, default ='Inserir')
+    local_rua= models.CharField(max_length=200, default ='Inserir')
+    local_longitude = models.IntegerField(default = 0)
+    local_latitude = models.IntegerField(default = 0)
 
 class Evento(models.Model):
     evento_nome = models.CharField(max_length=200)
-    evento_datahora = models.DateTimeField
+    evento_datahora = models.DateTimeField(default = now)
     evento_genero = models.CharField(max_length=200)
-    evento_valor = models.DecimalField
+    evento_valor = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
+   
 
-class Local(models.Model):
-    local_estacionamento = models.BooleanField
-    local_bairro = models.CharField(max_length=200)
-    local_cep = models.CharField(max_length=8)
-    local_rua=models.CharField(max_length=200)
-    local_longitude = models.IntegerField
-    local_latitude = models.IntegerField 
