76e664c1783a3525f3d0d3ed9c544ac5fbaf2dfe
Matheus da Silva Dantas <silva.dantas2@escolar.ifrn.edu.br>
2023-11-12 15:05:01 -0300

    alteração do nome das variaveis

--- mysite/vumbora/models.py
+++ mysite/vumbora/models.py
@@ -3,27 +3,33 @@ from django.utils.timezone import now
 
 # Create your models here.
 class Usuario(models.Model):
-    usuario_nome = models.CharField(max_length=200, default='SOME STRING')
-    usuario_email = models.EmailField(max_length=200, default='SOME STRING')
-    usuario_tipo = models.CharField(max_length=14,default = 'F' ,choices=[
+    nome = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    tipo = models.CharField(max_length=14,default = 'F' ,choices=[
         ('F', 'Física'),
         ('J', 'Jurídica')
     ])
-    
+    def __str__(self):
+        return self.nome
 
 class Local(models.Model):
-    local_nome = models.CharField(max_length=200, default= 'Nome')
-    local_bairro = models.CharField(max_length=200)
-    local_cep = models.CharField(max_length=8, default ='Inserir')
-    local_rua= models.CharField(max_length=200, default ='Inserir')
-    local_longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    local_latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    local_estacionamento = models.BooleanField(default =True)
+    nome = models.CharField(max_length=200)
+    bairro = models.CharField(max_length=200)
+    cep = models.CharField(max_length=8)
+    rua= models.CharField(max_length=200)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    estacionamento = models.BooleanField(default =True)
+    def __str__(self):
+        return self.nome
 
 class Evento(models.Model):
-    evento_nome = models.CharField(max_length=200)
-    evento_datahora = models.DateTimeField(default = now)
-    evento_genero = models.CharField(max_length=200)
-    evento_valor = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
-    evento_local = models.ForeignKey(Local, on_delete=models.CASCADE, null=True)
+    nome = models.CharField(max_length=200)
+    info = models.CharField(max_length=200, null=True)
+    datahora = models.DateTimeField(default = now)
+    genero = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
+    local = models.ForeignKey(Local, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.nome
 
