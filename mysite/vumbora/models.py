4480166ac33fdfba47f395d58c4af3c13fee22fc
Matheus da Silva Dantas <20222014040018@ifrn.local>
2023-11-14 12:46:17 -0300

    modificando

--- mysite/vumbora/models.py
+++ mysite/vumbora/models.py
@@ -5,16 +5,17 @@ from django.utils.timezone import now
 class Usuario(models.Model):
     nome = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    tipo = models.CharField(max_length=14,default = 'F' ,choices=[
+    tipo = models.CharField(max_length=1,default = 'F' ,choices=[
         ('F', 'Física'),
         ('J', 'Jurídica')
     ])
+    identidade = models.CharField(max_length =14)
     def __str__(self):
         return self.nome
 
 class Local(models.Model):
     nome = models.CharField(max_length=200)
-    bairro = models.CharField(max_length=200, choices=[
+    bairro = models.CharField(max_length=1, choices=[
         ('R', 'Ribeira'),
         ('P', 'Ponta Negra'),
         ('T', 'Tirol'),
@@ -22,15 +23,16 @@ class Local(models.Model):
     ])
     cep = models.CharField(max_length=20)
     rua= models.CharField(max_length=200)
+    numero = models.CharField(max_length=4, blank = True)
     estacionamento = models.BooleanField(default =True)
     def __str__(self):
         return self.nome
 
 class Evento(models.Model):
     nome = models.CharField(max_length=200)
-    info = models.CharField(max_length=200, null=True)
+    descricao = models.CharField(max_length=200, null=True)
     datahora = models.DateTimeField(default = now)
-    genero = models.CharField(max_length=200,choice =[
+    genero = models.CharField(max_length=200,choices =[
         ('E', 'Exposição'),
         ('F', 'Festa'),
         ('G', 'Gastronomia'),
