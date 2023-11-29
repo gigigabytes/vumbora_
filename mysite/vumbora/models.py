3863ee0e78fa5c576791e7372ca6eed34b476fbf
Matheus da Silva Dantas <silva.dantas2@escolar.ifrn.edu.br>
2023-11-29 00:07:54 -0300

    inicio do css

--- mysite/vumbora/models.py
+++ mysite/vumbora/models.py
@@ -28,13 +28,13 @@ class Local(models.Model):
     cep = models.CharField(max_length=20)
     rua= models.CharField(max_length=200)
     numero = models.CharField(max_length=4, blank = True)
-    estacionamento = models.BooleanField(default =True)
+    estacionamento = models.BooleanField(default = True)
     def __str__(self):
         return self.nome
      
 class Evento(models.Model):
     nome = models.CharField(max_length=200)
-    descricao = models.CharField(max_length=200, null=True)
+    descricao = models.CharField(max_length=200, null = True)
     datahora = models.DateTimeField(default = now)
     genero = models.CharField(max_length=1,choices =[
         ('E', 'Exposição'),
@@ -44,9 +44,9 @@ class Evento(models.Model):
         ('T','Teatro'),  
     ])
     valor = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
-    local = models.ForeignKey(Local, on_delete=models.CASCADE, null=True)
-    arte = models.ImageField(upload_to='images/', null=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
+    local = models.ForeignKey(Local, on_delete=models.CASCADE, null = True)
+    arte = models.ImageField(upload_to='images/', null = True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null = True)
     def __str__(self):
         return self.nome
 
