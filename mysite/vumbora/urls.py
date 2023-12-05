eac93c20f413baf9cd52744633e96eb60f573d1e
Maria Giovanna Lino dos Santos <20211014040040@ifrn.local>
2023-12-05 17:32:19 -0300

    tentando implementar pesquisa-evento

--- mysite/vumbora/urls.py
+++ mysite/vumbora/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import pesquisar_eventos
+#preciso colocar dessa forma? 
 
 app_name = 'vumbora'
 urlpatterns = [
     path('', views.index, name='index'),
     path('vumbora/<int:evento_id>/', views.details, name='details'),
+    path('pesquisar_eventos/', views.pesquisar_eventos, name='pesquisar_eventos'),
+    #url está correta?
 ]
--- mysite/vumbora/views.py
+++ mysite/vumbora/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Evento
+from django import forms
+
 
 # Create your views here.
 def index (request):
@@ -12,3 +14,17 @@ def index (request):
 def details (request, evento_id):
     evento = Evento.objects.get(pk=evento_id)
     return render(request,'vumbora/detail.html',{'evento':evento})
+
+def pesquisar_eventos(request):
+    if request.method == 'GET':
+        form = forms.Form(request.GET)
+        if form.is_valid():
+            termo_pesquisa = form.cleaned_data['termo_pesquisa']
+            eventos = Evento.objects.filter(nome__icontains=termo_pesquisa)
+        else:
+            eventos = Evento.objects.all()
+    else:
+        form = forms.Form()
+        eventos = Evento.objects.all()
+
+    return render(request, 'resultado_pesquisa.html', {'form': form, 'eventos': eventos})
