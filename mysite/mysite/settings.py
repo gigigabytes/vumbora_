37d8864aa0025f64a112774f61d6eb37bdbb4b71
Leonardo Teixiera Martins <leonardo.t@escolar.ifrn.edu.br>
2023-12-07 13:44:51 -0300

    listagem dos eventos da semana funcionando

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,13 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 # Ver uma forma de solução
 
 # Static files (CSS, JavaScript, Images)
