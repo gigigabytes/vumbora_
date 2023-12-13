e05398a568c25636c07bc1aa9c0262f41d78e71e
Rafael Silveira <129684440+RafaelSilveira-94@users.noreply.github.com>
2023-12-13 14:43:49 -0300

    corrigindo pós merge

--- mysite/vumbora/models.py
+++ mysite/vumbora/models.py
@@ -28,20 +28,8 @@ class Bairro(models.Model):
     def __str__(self):
         return self.nome
 
-class Bairro(models.Model):
-    nome = models.CharField(max_length= 200)
-    zona = models.CharField(max_length=2, choices=[
-        ('ZN', 'Zona Norte'),
-        ('ZS','Zona Sul'),
-        ('ZL', 'Zona Leste'),
-        ('ZO', 'Zona Oeste')
-    ])
-    def __str__(self):
-        return self.nome
-
 class Local(models.Model):
     nome = models.CharField(max_length=200)
-    bairro = models.ForeignKey(Bairro,on_delete=models.CASCADE, null=True)
     bairro = models.ForeignKey(Bairro,on_delete=models.CASCADE, null=True)
     cep = models.CharField(max_length=20)
     rua= models.CharField(max_length=200)
@@ -49,12 +37,10 @@ class Local(models.Model):
     estacionamento = models.BooleanField(default =True)
     class Meta:
         verbose_name_plural = "Locais"
-    class Meta:
-        verbose_name_plural = "Locais"
     def __str__(self):
         return self.nome
 
-class Genero(models.Model):
+
 class Genero(models.Model):
     genero = models.CharField(max_length=1,choices =[
         ('E', 'Exposição'),
@@ -68,17 +54,6 @@ class Genero(models.Model):
     def __str__(self):
         return self.genero
     
-
-class Evento(models.Model):
-    nome = models.CharField(max_length=200)
-    descricao = models.CharField(max_length=200, null=True)
-    datahora = models.DateTimeField(default = now)
-    class Meta:
-        verbose_name_plural = "Gêneros"
-    def __str__(self):
-        return self.genero
-    
-
 class Evento(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.CharField(max_length=200, null=True)
@@ -87,29 +62,12 @@ class Evento(models.Model):
     local = models.ForeignKey(Local, on_delete=models.CASCADE, null=True)
     arte = models.ImageField(upload_to='images/', null=True, blank = True)
     genero = models.ManyToManyField(Genero)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True, blank = True)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE,null=True)
-    genero = models.ManyToManyField(Genero)
     def __str__(self):
         return self.nome
     def img_preview(self):
         return mark_safe(f'<img src = "{self.product_img.url}" width = "{self.product.width}" height="{self.product.height}"/>')
     
-
-class Comenta(models.Model):
-    comentario = models.CharField(max_length=200)
-    data_hora = models.DateTimeField(default=now)
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE,null=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE,null=True)
-    class Meta:
-        verbose_name_plural = "Comentários"
-
-class Images(models.Model):
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name="evento_images", null = True)
-    class Meta:
-        verbose_name_plural = "Images"
-# python -m pip install Pill
 
 class Comenta(models.Model):
     comentario = models.CharField(max_length=200)
