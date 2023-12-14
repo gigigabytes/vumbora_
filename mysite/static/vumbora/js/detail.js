f40f3940765c716b520c4c58261d6060faee3fa7
Rafael Silveira <129684440+RafaelSilveira-94@users.noreply.github.com>
2023-12-14 01:37:09 -0300

    primeira versão carrossel

---
+++ mysite/static/vumbora/js/detail.js
@@ -0,0 +1,8 @@
+$('#carouselExampleControls').carousel();
+$('#prevButton').click(function() {
+    $('.carousel').slick('prev');
+  });
+  
+  $('#nextButton').click(function() {
+    $('.carousel').slick('next');
+  });
